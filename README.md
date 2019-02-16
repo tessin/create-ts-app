62cf973409e8a8ef5dcdc1f1cd0c1966a102ea26
John Leidegren <john@leidegren.se>
2019-02-16 10:11:48 +0100

    First commit
    Allow TypeScript projects to be created using yarn create

---
+++ README.md
@@ -0,0 +1,7 @@
+# Create a new TypeScript project
+
+Initialize and configure a TypeScript project for targeting JavaScript ES2017 and CommonJS.
+
+## Usage
+
+> `yarn create @tessin/ts-app <dirname>`
---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import fs = require("fs");
+
+import { resolve, join } from "path";
+import { spawnSync } from "child_process";
+
+const argv = process.argv.slice(2);
+
+if (!(0 < argv.length)) {
+  console.error(`usage:\n  yarn create @tessin/ts-app <dirname>`);
+  process.exit(1);
+}
+
+const dir = resolve(argv[0]);
+
+fs.mkdirSync(dir);
+
+// ================
+// init
+
+spawnSync("yarn init -y && yarn add -D typescript @types/node", {
+  shell: true,
+  cwd: dir,
+  stdio: "inherit"
+});
+
+// ================
+// package.json
+
+type PackageJson = {
+  name: string;
+  version: string;
+  main: string;
+  license: string;
+  devDependencies: {
+    [key: string]: string;
+  };
+  scripts?: {
+    [key: string]: string;
+  };
+  files?: string[];
+};
+
+const packageJsonFilename = join(dir, "package.json");
+
+const packageJson = JSON.parse(
+  fs.readFileSync(packageJsonFilename, "utf8")
+) as PackageJson;
+
+packageJson.main = "./lib/index.js";
+
+packageJson.scripts = {
+  start: "tsc -d -w",
+  build: "tsc -d",
+  prepublishOnly: "yarn build"
+};
+
+packageJson.files = ["lib/"];
+
+fs.writeFileSync(packageJsonFilename, JSON.stringify(packageJson, null, 2));
+
+// ================
+// tsconfig.json
+
+const tsconfigJson = fs.readFileSync(
+  join(__dirname, "../tsconfig.json"),
+  "utf8"
+);
+
+fs.writeFileSync(join(dir, "tsconfig.json"), tsconfigJson);
+
+// ================
+// .gitignore
+
+const gitignore: string[] = ["lib", "", "node_modules", "", "*.log"];
+
+fs.writeFileSync(join(dir, ".gitignore"), gitignore.join("\n"));
+
+// ================
+// .prettierrc.json
+
+const prettierrc = {
+  semi: false
+};
+
+fs.writeFileSync(
+  join(dir, ".prettierrc.json"),
+  JSON.stringify(prettierrc, null, 2)
+);
+
+// ================
+// index.ts
+
+fs.mkdirSync(join(dir, "src"));
+fs.writeFileSync(join(dir, "src", "index.ts"), "");
